5892ec2d10c4c5e6a1c5c7b8b297b2ac6614b305
Rails Group <akhosla@berkeley.edu>
2015-11-17 01:16:24 +0000

    Made some models

--- Lum/db/schema.rb
+++ Lum/db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117000836) do
+ActiveRecord::Schema.define(version: 20151117011358) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +43,8 @@ ActiveRecord::Schema.define(version: 20151117000836) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.integer  "year_started"
+    t.integer  "year_ended"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

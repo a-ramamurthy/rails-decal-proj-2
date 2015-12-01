f4d1385518b4a5c87e29b84b9a5155ed0809fb58
Kangsik Kevin Lee <kleekich@gmail.com>
2015-12-01 03:18:08 -0800

    Deleting post is almost done: we just need to figure out how to deal with association with post_tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body)
-    
+
  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   resources :users, :posts
 
   get "posts", to: "posts#index"
+  post "posts", to: "posts#create", as: :create
   get "posts/new", to: "posts#new", as: 'new'
   get "posts/:id", to: "posts#show"
-  post "posts", to: "posts#create", as: :create
-  get "posts/:id/edit", to: "posts#edit"
   put "posts/:id", to: "posts#update", as: 'update_post'
-  delete "posts/:id", to: "posts#destroy"
+  delete "posts", to: "posts#destroy", as: 'destroy_post'
+
+  get "posts/:id/edit", to: "posts#edit"
+
+
 end
